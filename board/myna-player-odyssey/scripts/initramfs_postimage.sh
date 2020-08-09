92c2fba3d87b7dfd042d5119598146e5f62ab70f
Xogium <contact@xogium.me>
2020-08-09 15:17:38 +0200

    board/MynaPlayer odyssey: removed initramfs.
    This build no longer requires an initramfs. It has hence been removed entirely. The associated defconfig is deleted, as are the postbuild and postimage scripts and overlays. The patches have been relocated to the rootfs_patches directory. Also worth noting is that the machine-id setup is entirely gone from the build system.

--- board/myna-player-odyssey/scripts/initramfs_postimage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# copy kernel, bootloader and device tree to a well known location
-
-mkdir -p "${BASE_DIR}"/../kernel
-cp -a "${BINARIES_DIR}"/zImage \
-	"${BINARIES_DIR}"/stm32mp157c-odyssey.dtb \
-	"${BINARIES_DIR}"/barebox.bin \
-	"${BASE_DIR}"/../kernel
