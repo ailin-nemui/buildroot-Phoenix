265e657796ef2b763ba10c52829bedb56733b26c
Xogium <contact@xogium.me>
2020-09-07 13:19:27 -0400

    board/MynaPlayer odyssey: changed the way the file name and metadata of the images are constructed.
    Currently, the envsubst tool substitutes the system date and a unique build ID generated with uuidgen.
    
    This commit changes both the template for genimage and the post-image rootfs script to use git describe instead.
    
    Also mentions the new naming scheme for the rauc bundles in the readme.
    
    Closes #20.

--- board/myna-player-odyssey/scripts/rootfs_postimage.sh
+++ board/myna-player-odyssey/scripts/rootfs_postimage.sh
@@ -8,16 +8,14 @@ fallocate -l 2m "${BINARIES_DIR}/rauc.ext4"
 mkfs.ext4 -F "${BINARIES_DIR}/rauc.ext4"
 
 # prepare for genimage
-
+cd "${BINARIES_DIR}"
 export GENIMAGE_TMP=${BR2_EXTERNAL_MynaPlayer_PATH}/board/myna-player-odyssey/utilities/genimage.tmp
 GENIMAGE_CFG="${BR2_EXTERNAL_MynaPlayer_PATH}/board/myna-player-odyssey/utilities/genimage.cfg"
-export GENIMAGE_DATE=$(date -Idate)
 export GENIMAGE_USER=$(whoami)
 export GENIMAGE_HOST=$(hostname)
-export GENIMAGE_BUILD_ID=$(uuidgen)
+export GENIMAGE_BUILD_ID=$(git describe --dirty --broken)
 
 envsubst \
-\$GENIMAGE_DATE,\
 \$GENIMAGE_USER,\
 \$GENIMAGE_HOST,\
 \$GENIMAGE_BUILD_ID \
