afb8dcb2d24f782339c47a062562875a8029589d
Xogium <contact@xogium.me>
2021-02-14 10:11:43 -0500

    board/Phoenix: create tarball and ext4 filesystem containing kernel, device tree and bootspecs files.

--- board/Phoenix/scripts/rootfs_postimage.sh
+++ board/Phoenix/scripts/rootfs_postimage.sh
@@ -2,6 +2,29 @@
 
 set -e
 
+# copy boot specs, kernel and device tree to a well known location
+
+mkdir -p "${BINARIES_DIR}"/kernel
+cp -ar \
+	"${BR2_EXTERNAL_PHOENIX_PATH}"/board/Phoenix/utilities/bootspecs/loader \
+	"${BINARIES_DIR}"/stm32mp157c-odyssey.dtb \
+	"${BINARIES_DIR}"/zImage \
+	"${BINARIES_DIR}"/kernel
+
+# compress them into a tarball for rauc
+tar -cJf \
+	"${BINARIES_DIR}"/kernel.tar.xz \
+	-C "${BINARIES_DIR}"/kernel \
+	.
+
+# create a filesystem for the initial image containing them
+
+fallocate -l 8m \
+	"${BINARIES_DIR}"/kernel.ext4
+mkfs.ext4 -Fd \
+	"${BINARIES_DIR}"/kernel \
+	"${BINARIES_DIR}"/kernel.ext4
+
 # prepare for genimage
 cd "${BINARIES_DIR}"
 export GENIMAGE_TMP=${BR2_EXTERNAL_PHOENIX_PATH}/board/Phoenix/utilities/genimage.tmp
