431597d569a9b692097e8ad0b84ac25823f47481
Xogium <contact@xogium.me>
2021-12-14 17:59:43 +0100

    external.mk: added BR2_PHOENIX_PSLC_EMMC variable.
    This is only used by post-build and post-image scripts.

--- external.mk
+++ external.mk
@@ -2,6 +2,7 @@ include $(sort $(wildcard $(BR2_EXTERNAL_PHOENIX_PATH)/fs/*/*.mk))
 include $(sort $(wildcard $(BR2_EXTERNAL_PHOENIX_PATH)/packages/*/*.mk))
 include $(sort $(wildcard $(BR2_EXTERNAL_PHOENIX_PATH)/packages/perl-libs/*/*.mk))
 
+export BR2_PHOENIX_PSLC_EMMC=0
 export MODULES_PLACE = $(BASE_DIR)/../kmod
 export KERNEL_PLACE = $(BASE_DIR)/../kernel
 
