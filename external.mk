779628015291448ae7ba78eb79edbb9ef6282056
Xogium <contact@xogium.me>
2022-04-27 11:52:35 +0200

    external.mk: clean up this.
    There were unused variables and actions in this makefile, so lets clean them up.
    
    Also removed the no longer present packages/perl-libs directory.

--- external.mk
+++ external.mk
@@ -1,14 +1,7 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_PHOENIX_PATH)/fs/*/*.mk))
 include $(sort $(wildcard $(BR2_EXTERNAL_PHOENIX_PATH)/packages/*/*.mk))
-include $(sort $(wildcard $(BR2_EXTERNAL_PHOENIX_PATH)/packages/perl-libs/*/*.mk))
 
 export BR2_PHOENIX_PSLC_EMMC=0
-export MODULES_PLACE = $(BASE_DIR)/../kmod
-export KERNEL_PLACE = $(BASE_DIR)/../kernel
-
-pre-clean:
-	rm -rf $(MAGIC_PLACE)
-	rm -rf $(KERNEL_PLACE)
 
 rauc-fixups:
 	$(BR2_EXTERNAL_PHOENIX_PATH)/scripts/rauc_fixups.sh
